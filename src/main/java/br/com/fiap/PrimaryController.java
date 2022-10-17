d841f5398bc8ccdf2c3ad8cac9ff72d8dac5ec43
Joao <joaocarloslima@me.com>
2022-10-17 08:58:36 -0300

    feat: conexao

--- src/main/java/br/com/fiap/PrimaryController.java
+++ src/main/java/br/com/fiap/PrimaryController.java
@@ -1,12 +1,28 @@
 package br.com.fiap;
 
-import java.io.IOException;
-import javafx.fxml.FXML;
+import java.sql.SQLException;
+
+import br.com.fiap.conexao.ConnectionFactory;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
 
 public class PrimaryController {
 
-    @FXML
-    private void switchToSecondary() throws IOException {
-        App.setRoot("secondary");
+    public void salvar(){
+        try {
+            var conexao = ConnectionFactory.getConnection();
+            System.out.println("conectado");
+            conexao.close();
+        } catch (SQLException e) {
+            mostrarErro(e);
+            e.printStackTrace();
+        }
     }
+
+    private void mostrarErro(SQLException e) {
+        var alerta = new Alert(AlertType.ERROR);
+        alerta.setContentText(e.getMessage());
+        alerta.show();
+    }
+
 }
